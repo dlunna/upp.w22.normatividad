ec23e2914c7373904b7eac1463ac3ee8db2e6e63
d4v <dlunna@gmail.com>
2025-05-20 21:43:44 -0600

    se agregan documentos de tics y se actualiza a libreria 25

--- interna.php
+++ interna.php
@@ -1,16 +1,10 @@
-<?php $titulo = "Normatividad Interna" ?>
-<?php $banner = "interna" ?>
-
 <!DOCTYPE html>
-<html lang="es">
+<html lang="mx">
 
 <head>
-  <?php
-    include ('./dotk/00head/analitycs.html');
-    include ('../lib20/core/10cabecera.html');
-    include ('../lib20/core/20estilos.html');
-    include ('../lib20/hgo/zoram_style_header.html');
-  ?>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="../lib25/CSS/style.css">
 </head>
 
 <body>
@@ -18,45 +12,33 @@
   <header>
     <!-- Logos e imagen de hidalgo -->
     <?php
-    //include ('../lib18/seph/hf/header.html');
-  	//include ('../lib20/hgo/header.html');
-  	?>
-    <?php include ('../lib20/hgo/veda_navbar.html'); ?>
-    <!-- Slider principal -->
-    <?php include ('./dotk/01header/01carousel-principal/carousel.html'); ?>
+    include('../lib25/header.html');
+    ?>
   </header>
-
-  <?php include ('./dotk/01header/02begajoso/menu-pegajoso.html'); ?>
 
   <main>
 
-    <?php include ('./dotk/05main/norm-interna.html'); ?>
-
-    <div class="container marketing">
-      <a id="mapadesitio"></a>
-      <br />
-      <hr class="featurette-divider">
-      <!-- Menu con todos los sitios y mini sitios de la página -->
-      <?php include ('../lib20/core/60menufooter.php'); ?>
+    <br>
+    <div class="row mb-3 text-center">
+      <div class="col-md-8 themed-grid-col">
+        <a class="btn btn-primary d-inline-flex align-items-center" href="./interna.php">Ver Normatividad Interna</a>
+      </div>
+      <div class="col-md-4 themed-grid-col">
+        <a class="btn btn-primary d-inline-flex align-items-center" href="./externa.php">Ver Normatividad Externa</a>
+      </div>
     </div>
+
+    <?php include('./dotk/05main/norm-interna.html'); ?>
+
   </main>
 
   <footer>
-    <!-- footer de gobierno del estado -->
-    <?php //include ('../lib20/seph/hf/footer.html'); ?>
-    <?php //include ('../lib20/hgo/footer.html'); ?>
-    <?php include ('../lib20/hgo/veda_footer.html'); ?>
+    <?php include('../lib25/footer.html'); ?>
   </footer>
 
   <?php
-    include ('../lib20/core/80js.html');
-    //include ('../lib20/hgo/zoram_js_footer.html');
+  include('../lib25/80js.html');
   ?>
-
-  <!-- Script para dar la animacion al boton flotante que lleva arriba
-  <a href="#" class="back-to-top">Volver arriba</a>
-  <script src="../lib20/core/31script.js"></script>-->
-
 
 </body>
 
